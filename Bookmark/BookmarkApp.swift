592175626261190a31253aed3f214fa674ba906a
zamong25 <wool0602@naver.com>
2021-05-07 13:10:08 +0900

    Initial Commit

---
+++ Bookmark/BookmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkApp.swift
+//  Bookmark
+//
+//  Created by PCW on 2021/05/07.
+//
+
+import SwiftUI
+
+@main
+struct BookmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
