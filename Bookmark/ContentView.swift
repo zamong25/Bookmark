def0153391732403203099c2fe404b247dc48b4b
zamong25 <wool0602@naver.com>
2021-05-07 13:33:16 +0900

    Create Favorites with SwiftUI

--- Bookmark/ContentView.swift
+++ Bookmark/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isFav = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Toggle(isOn: $isFav){
+                    Text("Show Favorites only")
+                }
+                List{
+                    HStack{
+                        Text("Dogs")
+                        Spacer()
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                    }
+                if isFav == false{
+                    HStack{
+                        Text("Dogs")
+                        Spacer()
+                        Image(systemName: "star.fill")
+                    }
+                    }else {
+                        EmptyView()
+                    }
+                    HStack{
+                        Text("Dogs")
+                        Spacer()
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                    }
+                    HStack{
+                        Text("Dogs")
+                        Spacer()
+                        Image(systemName: "star.fill")
+                    }
+                }
+            }
+        }.font(.largeTitle)
     }
 }
 
